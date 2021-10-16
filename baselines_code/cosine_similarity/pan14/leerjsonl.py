af10f081573456f77e28de2bcd69f5f25681d53a
Iván Reyes <67800498+ivannpy@users.noreply.github.com>
2021-10-15 19:35:37 -0500

    Delete baselines_code directory

--- baselines_code/cosine_similarity/pan14/leerjsonl.py
+++
@@ -1,9 +0,0 @@
-import jsonlines
-import json
-with jsonlines.open('test-corpus/test/truth.jsonl') as r:
-    i=0
-    for o in r:
-        print(o)
-        i+=1
-        if i==2:
-            break
